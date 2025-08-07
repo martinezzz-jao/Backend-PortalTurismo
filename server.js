82fbae3a952ca7e6cc3eb9d43e0560aef81a091f
martinezzz-jao <joaovitordepaulamartinez@gmail.com>
2025-08-07 16:44:13 -0300

    updt

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({                            
-  origin: 'https://frontend-portal-turismo.vercel.app', // utilizar o link da vercel de vocês        
+  origin: 'https://frontend-portal-turismo-beta.vercel.app', // utilizar o link da vercel de vocês        
   methods: ['GET','POST','PUT','DELETE'],
   credentials: true
 }));
